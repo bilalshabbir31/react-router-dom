8a044e28b9f6f142a56f1efa7b2e07885c60808c
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-09-29 22:22:27 +0500

    create app context provider

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ import Home from './pages/Home';
 import Menu from './pages/Menu';
 import ContactUs from './pages/ContactUs';
 import Navbar from './components/Navbar';
+import { useState, useContext, createContext } from 'react';
+
+export const AppContext = createContext();
 
 function App() {
+
+
+  const [username, setUsername] = useState("Bilal");
+
   return (
     <div className="App">
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="*" element={<h1>Page Not Found</h1>} />
-        </Routes>
-      </Router>
+      <AppContext.Provider value={{username, setUsername}}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu/>} />
+            <Route
+              path="/contact-us"
+              element={<ContactUs/>}
+            />
+            <Route path="*" element={<h1>Page Not Found</h1>} />
+          </Routes>
+        </Router>
+      </AppContext.Provider>
     </div>
   );
 }
