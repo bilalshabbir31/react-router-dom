b79405e4cc1057ddc5afd4d72600612e8ad50fc1
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-10-08 17:32:38 +0500

    fetch data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,23 @@
 
+import { useQuery } from "@tanstack/react-query"
+import Axios from "axios";
 const Home = () => {
+
+  const { data, isLoading, refetch } = useQuery(["cat"], () => {
+    return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+  });
+
+  if (isLoading){
+    return <h1>Loading</h1>
+  }
+
   return (
-    <div>This is Home</div>
-  )
+    <div>
+      This is Home
+      <p>{data?.fact}</p>
+      <button onClick={refetch}>Update data</button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
