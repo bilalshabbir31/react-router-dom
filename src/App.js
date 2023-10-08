f0107819fbd6f07de37675b1c9aac3c56a00ff37
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-10-08 17:32:32 +0500

    use react-query + reactqueryclient

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import Home from './pages/Home';
 import Menu from './pages/Menu';
 import ContactUs from './pages/ContactUs';
 import Navbar from './components/Navbar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
+
+  const client = new QueryClient();
+
   return (
     <div className="App">
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="*" element={<h1>Page Not Found</h1>} />
-        </Routes>
-      </Router>
+      <QueryClientProvider client={client}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="*" element={<h1>Page Not Found</h1>} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
